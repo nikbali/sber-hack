22228f32d9f0351983a8adedf611100260b465d5
nibali <nikbali@mail.ru>
2019-10-24 10:44:27 +0300

    configure oracle connect

---
+++ src/main/java/com/sberbank/hack/controllers/DatabaseScannerController.java
@@ -0,0 +1,18 @@
+package com.sberbank.hack.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class DatabaseScannerController {
+
+
+    @GetMapping("/getDatabaseInfo")
+    public ResponseEntity getDatabaseInfo(){
+        return ResponseEntity.ok("O my");
+    }
+}
