4ab7486d783888618f82913c103a97f25d4e26c9
Sergey Zalizko <sergonas@gmail.com>
2019-10-24 17:05:51 +0300

    Unused imports

--- src/main/java/com/sberbank/hack/HackApplication.java
+++ src/main/java/com/sberbank/hack/HackApplication.java
@@ -3,9 +3,6 @@ package com.sberbank.hack;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
-import org.springframework.web.filter.CorsFilter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
