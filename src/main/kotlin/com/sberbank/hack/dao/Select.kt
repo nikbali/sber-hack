eb0817cd70ff172c0ad84078da67c673990dae00
Vsevolod Fitin <fitincontact@gmail.com>
2019-10-24 23:19:46 +0300

    sql

--- src/main/kotlin/com/sberbank/hack/dao/Select.kt
+++ src/main/kotlin/com/sberbank/hack/dao/Select.kt
@@ -11,14 +11,57 @@ import kotlin.collections.ArrayList
 @Component
 class Select {
     fun logFiles(connect: Connection): Collection<LogFile> {
-        val sql : String = "select * from v${'$'}archived_log where trunc(first_time) = to_date(sysdate)"
+        val sql: String = "select\n" +
+                "a.recid\n" +
+                ",a.stamp\n" +
+                ",a.name\n" +
+                ",a.first_change#\n" +
+                ",a.next_change#\n" +
+                ",a.first_time\n" +
+                ",a.next_time" +
+                "from v${'$'}archived_log a \n" +
+                "where trunc(a.first_time) = to_date(sysdate)\n"
         val preparedStatement: PreparedStatement = connect.prepareStatement(sql)
         val resultSet: ResultSet = preparedStatement.executeQuery()
 
         val logFileList = ArrayList<LogFile>()
 
-/*        while(resultSet.next()) {
-            logFileList.add(Operation(
+        while (resultSet.next()) {
+            logFileList.add(LogFile(
+                    resultSet.getLong("RECID"),
+                    resultSet.getLong("STAMP"),
+                    resultSet.getString("NAME"),
+                    resultSet.getLong("FIRST_CHANGE#"),
+                    resultSet.getLong("NEXT_CHANGE#"),
+                    resultSet.getLong("FIRST_TIME"),
+                    resultSet.getLong("NEXT_TIME")
+            ))
+        }
+        return logFileList
+    }
+
+    fun operations(connect: Connection): Collection<Operation> {
+        //V$LOGMNR_CONTENTS
+        val sql: String = "select \n" +
+                "t.scn\n" +
+                ",t.start_scn\n" +
+                ",t.start_timestamp\n" +
+                ",t.xid\n" +
+                ",t.sql_redo\n" +
+                ",t.sql_undo\n" +
+                ",t.info\n" +
+                ",t.redo_value\n" +
+                ",t.xid\n" +
+                " from data_lgmr t\n" +
+                //TODO
+                " where rownum < 11"
+        val preparedStatement: PreparedStatement = connect.prepareStatement(sql)
+        val resultSet: ResultSet = preparedStatement.executeQuery()
+
+        val operationList = ArrayList<Operation>()
+
+        while (resultSet.next()) {
+            operationList.add(Operation(
                     resultSet.getLong("SCN"),
                     resultSet.getLong("START_SCN"),
                     resultSet.getString("START_TIMESTAMP"),
@@ -28,41 +71,7 @@ class Select {
                     resultSet.getString("REDO_VALUE"),
                     resultSet.getString("XID")
             ))
-        }*/
-        return logFileList
-    }
-    fun operations(connect: Connection): Collection<Operation> {
-        //V$LOGMNR_CONTENTS
-        val sql: String = "select " +
-                "t.scn" +
-                ",t.start_scn" +
-                ",t.start_timestamp" +
-                ",t.xid" +
-                ",t.sql_redo" +
-                ",t.sql_undo" +
-                ",t.info" +
-                ",t.redo_value" +
-                ",t.xid" +
-                " from data_lgmr t" +
-                //TODO
-                " where rownum < 11"
-        val preparedStatement: PreparedStatement = connect.prepareStatement(sql)
-        val resultSet: ResultSet = preparedStatement.executeQuery()
-
-        val operationList = ArrayList<Operation>()
-
-        while(resultSet.next()) {
-            operationList.add(Operation(
-                resultSet.getLong("SCN"),
-                resultSet.getLong("START_SCN"),
-                resultSet.getString("START_TIMESTAMP"),
-                resultSet.getString("XID"),
-                resultSet.getString("SQL_REDO"),
-                resultSet.getString("SQL_UNDO"),
-                resultSet.getString("REDO_VALUE"),
-                resultSet.getString("XID")
-        ))
-    }
+        }
         return operationList
     }
 }
--- src/main/kotlin/com/sberbank/hack/dao/models/LogFile.kt
+++ src/main/kotlin/com/sberbank/hack/dao/models/LogFile.kt
@@ -1,4 +1,11 @@
 package com.sberbank.hack.dao.models
 
-class LogFile {
-}
+data class LogFile(
+        var recid: Long = 0,
+        var stamp: Long = 0,
+        var name: String = "",
+        var firstChange: Long = 0,
+        var nextChange: Long = 0,
+        var firstTime: Long = 0,
+        var nextTime: Long = 0
+)
