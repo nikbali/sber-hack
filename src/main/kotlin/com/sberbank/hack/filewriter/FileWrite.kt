9418e8e1908c8576c180430658bb7248f55f99f3
Sergey Zalizko <sergonas@gmail.com>
2019-10-24 14:20:06 +0300

    File writer

--- src/main/kotlin/com/sberbank/hack/filewriter/FileWrite.kt
+++ src/main/kotlin/com/sberbank/hack/filewriter/FileWrite.kt
@@ -3,21 +3,44 @@ package com.sberbank.hack.filewriter
 import java.io.BufferedWriter
 import java.io.File
 import java.io.FileWriter
+import java.nio.file.Files
+import java.nio.file.Paths
+import java.time.LocalDateTime
+import java.time.format.DateTimeFormatter
 
 object FileWrite {
-    val logFolderName = "C:\\Work\\Log\\"
-    val logFileName = "file.log"
-    val instructionFileName = "rpl.sql"
+    private const val logFolderName = "C:\\Work\\Log\\"
+    private const val logFileName = "file.log"
+    private const val instructionFileName = "rpl.sql"
+    private val dateFormatter = DateTimeFormatter.ofPattern("yy-MM-dd_HH-mm-ss")
 
-    fun writeInstruction(instruction: String) {
-
+    fun writeInstruction(instruction: String, txId: Long) {
+        val instructionFileName = logFolderName + instructionFileName
+        if (checkSizeLimit(instructionFileName)) rotateFile(instructionFileName)
+        val os = FileWriter(File(instructionFileName), true)
+        val writer = BufferedWriter(os)
+        writer.append("$instruction; -- txId=$txId")
+        writer.newLine()
+        writer.close()
     }
 
     fun writeLog(logEntry: String) {
-        val os = FileWriter(File(logFolderName + logFileName), true)
+        val logFilename = logFolderName + logFileName
+        if (checkSizeLimit(logFilename)) rotateFile(logFilename)
+        val os = FileWriter(File(logFilename), true)
         val writer = BufferedWriter(os)
         writer.append(logEntry)
         writer.newLine()
         writer.close()
     }
+
+    private fun rotateFile(filename: String): String {
+        val start = filename.substring(0, filename.length - 4)
+        val end = filename.substring(filename.length - 4)
+        val newFileName = start + "_" + dateFormatter.format(LocalDateTime.now()) + end
+        Files.move(Paths.get(filename), Paths.get(newFileName))
+        return newFileName;
+    }
+
+    private fun checkSizeLimit(filename: String): Boolean = File(filename).length() / 1024 / 1024 > 10
 }
--- src/main/kotlin/com/sberbank/hack/filewriter/LogService.kt
+++ src/main/kotlin/com/sberbank/hack/filewriter/LogService.kt
@@ -1,14 +1,23 @@
 package com.sberbank.hack.filewriter
 
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
+import com.google.common.collect.EvictingQueue
 import com.sberbank.hack.dto.Transaction
 import org.springframework.stereotype.Service
+import java.util.*
 
 @Service
 class LogService {
+    private val buffer = EvictingQueue.create<Transaction>(20)
+
+    fun getLast(): Collection<Transaction> = Collections.unmodifiableCollection(buffer)
+
     fun log(tx: Transaction) {
         val mapper = jacksonObjectMapper()
         val json = mapper.writeValueAsString(tx)
+        tx.instructions.forEach{FileWrite.writeInstruction(it.sql, tx.txId)}
         FileWrite.writeLog(json)
+
+        buffer.add(tx)
     }
 }
