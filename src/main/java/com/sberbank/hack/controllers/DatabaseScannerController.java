28746f14a6f0aa9e0207558bcd3d95af2000ff5e
Sergey Zalizko <sergonas@gmail.com>
2019-10-24 14:59:51 +0300

    GetDbInfo

--- src/main/java/com/sberbank/hack/controllers/DatabaseScannerController.java
+++ src/main/java/com/sberbank/hack/controllers/DatabaseScannerController.java
@@ -1,5 +1,6 @@
 package com.sberbank.hack.controllers;
 
+import com.sberbank.hack.dto.DbInfo;
 import com.sberbank.hack.dto.Instruction;
 import com.sberbank.hack.dto.Transaction;
 import com.sberbank.hack.filewriter.LogService;
@@ -7,9 +8,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.sql.DataSource;
+import java.sql.SQLException;
 import java.time.Instant;
 import java.util.Arrays;
 import java.util.Collection;
@@ -21,10 +23,12 @@ public class DatabaseScannerController {
 
     @Autowired
     private LogService logService;
+    @Autowired
+    private DataSource ds;
 
     @GetMapping("/getDatabaseInfo")
-    public ResponseEntity getDatabaseInfo(){
-        return ResponseEntity.ok("O my");
+    public ResponseEntity getDatabaseInfo() throws SQLException {
+        return ResponseEntity.ok(new DbInfo(ds.getConnection().getMetaData().getURL()));
     }
     @GetMapping("/getLastTransactions")
     public Collection<Transaction> getLastTxs() {
