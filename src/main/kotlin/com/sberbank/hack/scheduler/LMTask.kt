27b34c5771084fe0d88454e602af00451df6f216
Sergey Zalizko <sergonas@gmail.com>
2019-10-25 04:37:08 +0300

    Commit2

--- src/main/kotlin/com/sberbank/hack/scheduler/LMTask.kt
+++
@@ -1,7 +0,0 @@
-package com.sberbank.hack.scheduler
-
-data class LMTask(
-        val archLogFile: String,
-        val startPosition: Long,
-        val endPosition: Long
-)
--- src/main/kotlin/com/sberbank/hack/scheduler/ProduceExecuteService.kt
+++ src/main/kotlin/com/sberbank/hack/scheduler/ProduceExecuteService.kt
@@ -14,8 +14,8 @@ import java.sql.Connection
 import java.sql.Date
 import java.sql.DriverManager
 import java.util.*
+import java.util.concurrent.CompletableFuture
 import java.util.concurrent.ConcurrentLinkedQueue
-import java.util.concurrent.Executors
 import java.util.concurrent.atomic.AtomicBoolean
 
 /**
@@ -39,9 +39,6 @@ class ProduceExecuteService {
     @Autowired
     lateinit var select: Select
 
-    @Volatile
-    private var logTaskQueue = ConcurrentLinkedQueue<Operation>()
-
     fun enable() {
         isEnableProduce.set(true);
     }
@@ -51,85 +48,62 @@ class ProduceExecuteService {
     }
 
     fun execute() {
-        Locale.setDefault(Locale.ENGLISH)
-        val producer = Executors.newScheduledThreadPool(1)
-        val consumer = Executors.newScheduledThreadPool(1)
-        val connection = DriverManager.getConnection(
-                environment.getProperty("spring.datasource.url"),
-                environment.getProperty("spring.datasource.username"),
-                environment.getProperty("spring.datasource.password"))
+        CompletableFuture.runAsync {
+            Locale.setDefault(Locale.ENGLISH)
+            val connection = DriverManager.getConnection(
+                    environment.getProperty("spring.datasource.url"),
+                    environment.getProperty("spring.datasource.username"),
+                    environment.getProperty("spring.datasource.password"))
 
-        PreparationSelect.build(connection)
-        val logFiles = select.logFiles(connection, Date(System.currentTimeMillis() - 100000))
-//        val scn = select.initialSCN(connection)
-
-        PreparationSelect.addLogFile(connection, logFiles.name)
-        PreparationSelect.startLogMnr(connection)
-
-        producer.submit(Producer(connection))
-        consumer.submit(Consumer(connection))
+            log.info("Dict build started")
+            PreparationSelect.build(connection)
+            log.info("Dict dict finished")
+            val logFile = select.logFiles(connection, Date(System.currentTimeMillis() - 100000))
+            log.info("Arch log file ${logFile.name}")
+            log.info("Add log file started")
+            PreparationSelect.addLogFile(connection, logFile.name)
+            log.info("Start log miner")
+            PreparationSelect.startLogMnr(connection)
+            log.info("Log miner started")
+            doWork(connection)
+        }
     }
 
-    internal inner class Producer(private val connection: Connection) : Runnable {
+    fun doWork(conn: Connection) {
+        var currentScn : Long = FileWriter.readCdn().cdn
 
-        override fun run() {
+        if(currentScn == 0L) {
+            currentScn = select.initialSCN(conn)
+        }
+        log.info("Current scn = $currentScn")
 
-            var currentScn : Long = FileWriter.readCdn().cdn
+        while (true) {
+            if (isEnableProduce.get()) {
+                log.info("Reading mined data")
+                val operations = select.operations(conn, currentScn, 10)
 
-            if(currentScn == 0L){
-                currentScn = select.initialSCN(connection)
-            }
-
-            while (true) {
-                if (isEnableProduce.get()) {
-
-                    val operations = select.operations(connection, currentScn, 10)
-
-                    if (operations.isNotEmpty()) {
-                        val lastOperation: Operation? = getLastElement(operations)
-                        currentScn = lastOperation?.scn ?: currentScn
-                        logTaskQueue.addAll(operations)
-                        log.info("add Logs in Queue")
-                    } else {
-                        log.info("data empty")
-                        Thread.sleep(1000)
+                if (operations.isNotEmpty()) {
+                    val lastOperation: Operation? = getLastElement(operations)
+                    currentScn = lastOperation?.scn ?: currentScn
+                    for (op in operations) {
+                        log.info(op.toString())
+                        logService.log(op)
+                        log.info("Write to file tx with xid" + op.xid)
                     }
-
+                    log.info("add Logs in Queue")
                 } else {
-                    //записываем последний cnd
-                   FileWriter.writeCdn(CdnDto(currentScn))
+                    log.info("data empty. sleeping")
+                    Thread.sleep(1000)
                 }
+            } else {
+                //записываем последний cnd
+                FileWriter.writeCdn(CdnDto(currentScn))
+                log.info("Written current scn = $currentScn")
             }
         }
     }
 
-    internal inner class Consumer(private val connection: Connection) : Runnable {
-
-        override fun run() {
-
-            while (true) {
-
-                if (!isEnableProduce.get() && logTaskQueue.isEmpty()) {
-
-                    //завершаем поток и закрываем конект
-                    connection.close()
-                    log.info("End FIFO service")
-                    break;
-
-                } else {
-
-                    if (!logTaskQueue.isEmpty()) {
-                        val operation: Operation = logTaskQueue.poll()
-                        log.info(operation.toString())
-                        logService.log(operation)
-                        log.info("Write to file" + operation.xid)
-                    }
-                }
-            }
-        }
-    }
-
-    fun <T> getLastElement(elements: Iterable<T>): T? {
+    private fun <T> getLastElement(elements: Iterable<T>): T? {
         var lastElement: T? = null
 
         for (element in elements) {
